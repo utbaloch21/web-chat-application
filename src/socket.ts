7e1a0f190e4975336a4a28053965c34bc37ea6cb
Osama <usamatariq10048@gmail.com>
2023-10-17 12:31:27 +0500

    changed port to 8080

--- src/socket.ts
+++ src/socket.ts
@@ -13,7 +13,7 @@ interface SocketData extends Socket {
 // https://web-chat-application-ehfo6idgb-utbaloch21s-projects.vercel.app
 const SERVER_IP =
   "web-chat-application-ehfo6idgb-utbaloch21s-projects.vercel.app"; // e.g., '10.13.1.215'
-const SERVER_PORT = 3001;
+const SERVER_PORT = 8080;
 const socket: SocketData = io(`https://${SERVER_IP}:${SERVER_PORT}`, {
   autoConnect: false,
 });
