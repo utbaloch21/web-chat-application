2678a91dfe0aad54808dee3b2ae4fdee2364372b
Osama <usamatariq10048@gmail.com>
2023-10-17 12:12:23 +0500

    Added url

--- src/socket.ts
+++ src/socket.ts
@@ -10,9 +10,11 @@ interface SocketData extends Socket {
   username?: string;
   sessionID?: string;
 }
-const SERVER_IP = "10.13.1.215"; // e.g., '10.13.1.215'
+// https://web-chat-application-ehfo6idgb-utbaloch21s-projects.vercel.app
+const SERVER_IP =
+  "web-chat-application-ehfo6idgb-utbaloch21s-projects.vercel.app"; // e.g., '10.13.1.215'
 const SERVER_PORT = 3001;
-const socket: SocketData = io(`http://${SERVER_IP}:${SERVER_PORT}`, {
+const socket: SocketData = io(`https://${SERVER_IP}:${SERVER_PORT}`, {
   autoConnect: false,
 });
 
